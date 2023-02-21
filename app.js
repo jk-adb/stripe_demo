ef4edd1bc0eb3766de23b7c9d6bde2b1b44f130f
jk <jkimura0124@yahoo.co.jp>
2023-02-21 15:01:22 +0900

    20230232-pm-03

--- app.js
+++ app.js
@@ -2,19 +2,6 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
-/*
-var nedb = require('nedb');
-var db = {};
-db.products = new nedb({
-    filename: 'data/products.db'
-});
-db.transactions = new nedb({
-    filename: 'data/transactions.db'
-});
-db.products.loadDatabase();
-db.transactions.loadDatabase();
-*/
-
 var bodyParser = require("body-parser");
 
 app.use("/static", express.static('static'));
@@ -22,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/admin", require("./admin.js"));
+//app.use("/admin", require("./admin.js"));
 
 app.use("/", require("./store.js"));
 
